8a1442b0169502bbb739560e90ba580bdd7d55b8
nulikartik <nulikarthik@gmail.com>
2018-04-10 00:03:15 -0700

    Update CalendarGroup.js

--- src/Components/CalendarGroup.js
+++ src/Components/CalendarGroup.js
@@ -7,13 +7,14 @@ class CalendarGroup extends Component{
     }
     render(){
         var date = new Date(this.props.date);
-        var startDate = date.getDate();//08
+        var startDate = date.getUTCDate();//08
         var fromMonth = date.getMonth();//04
         var range = this.props.range;//23
         var dupRange = range;
         var year = date.getFullYear();
         const countryCode = this.props.countryCode;
-       
+        var as = new Date(date.getMonth()+1+"/01/"+date.getFullYear());
+        var ax = as.getDay();
         var months = ["Januay","February","March","April","May","June","July","August","September","October","November","December"];
         
         var monthDates = [31,28,31,30,31,30,31,31,30,31,30,31];
@@ -23,17 +24,15 @@ class CalendarGroup extends Component{
         var cal = [];
         var check = true;
         var endDate = monthDates[fromMonth];//30
-        var sum = startDate + parseInt(range,10); //31
-        console.log("Sum: ",sum);
-        console.log("Range: ",range);
-        console.log("fromMOnth",fromMonth);
-        if(sum >= endDate){console.log("DaUpdated",date);
-            cal.push(<CalendarBlock countryCode={countryCode} startDate={startDate} toDate={endDate} range={range} day={date.getDay()} month={months[fromMonth]+" - "+year} getMonth={fromMonth} />);
+        var sum = startDate + parseInt(range,10); //32
+        if(sum >= endDate){
+            cal.push(<CalendarBlock countryCode={countryCode} startDate={startDate} toDate={endDate} range={range} day={ax} month={months[fromMonth]+" - "+year} getMonth={fromMonth} />);
             range -= endDate - startDate;
             date.setDate(date.getDate() + endDate - startDate);
+            range--;
         }
-        else{console.log("Booyah: ",range);
-            cal.push(<CalendarBlock countryCode={countryCode} startDate={startDate} toDate={endDate} range={range} day={date.getDay()} month={months[fromMonth]+" - "+year} getMonth={fromMonth}/>);
+        else{
+            cal.push(<CalendarBlock countryCode={countryCode} startDate={startDate} toDate={endDate} range={range} day={ax} month={months[fromMonth]+" - "+year} getMonth={fromMonth}/>);
             range = 0;
         }
         while(range > 0){
@@ -49,13 +48,15 @@ class CalendarGroup extends Component{
             fromMonth++;
             }
             var nextMonthDays = monthDates[fromMonth];
-            if(range >= nextMonthDays){console.log("DaUpdated",date);
-                cal.push(<CalendarBlock countryCode={countryCode} startDate={1} toDate={nextMonthDays} range={range} day={date.getDay()} month={months[fromMonth]+" - "+year} getMonth={fromMonth}/>);
+            as = new Date(fromMonth+1+"/01/"+date.getFullYear());
+            ax = as.getDay();
+            if(range >= nextMonthDays){
+                cal.push(<CalendarBlock countryCode={countryCode} startDate={1} toDate={nextMonthDays} range={range} day={ax} month={months[fromMonth]+" - "+year} getMonth={fromMonth}/>);
                 range -= nextMonthDays;
                 date.setDate(date.getDate() + nextMonthDays);
             }
             else{
-                cal.push(<CalendarBlock countryCode={countryCode} startDate={1} toDate={nextMonthDays} range={range} day={date.getDay()} month={months[fromMonth]+" - "+year} getMonth={fromMonth}/>);
+                cal.push(<CalendarBlock countryCode={countryCode} startDate={1} toDate={nextMonthDays} range={range} day={ax} month={months[fromMonth]+" - "+year} getMonth={fromMonth}/>);
                 range = 0;
             }
         }
